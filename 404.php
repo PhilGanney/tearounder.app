bce7921af895de6db2202a5dccbdfdb9174504e7
Phil Ganney <philipganney@gmail.com>
2022-05-13 13:32:04 +0100

    ugggh slashes needed
    yup, I'm officially re-learning php at this point

--- 404.php
+++ 404.php
@@ -1,15 +1,13 @@
 <?php
 	if (isset( $_SERVER['REQUEST_URI'])) {
 		switch (pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME)){
-			case "ThreeCups":
+			case "/ThreeCups":
 				header('location:https://tearounder.app/ThreeCups/menu.html');
 				break;
-			case "TheLoremIpsum":
+			case "/TheLoremIpsum":
 				header('location:https://tearounder.app/TheLoremIpsum/menu.html');
 				break;
 			default:
-				echo $_SERVER['REQUEST_URI'];
-				echo pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME);
 		}
 	} else {
 		header('location:https://tearounder.app/index.html');
