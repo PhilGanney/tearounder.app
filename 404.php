8df115da07560a19a20e0ca2000d1367644f1525
Phil Ganney <philipganney@gmail.com>
2022-05-13 13:44:41 +0100

    404s: missing info page would show generic one

--- 404.php
+++ 404.php
@@ -1,5 +1,9 @@
 <?php
 	if (isset( $_SERVER['REQUEST_URI'])) {
+		if (pathinfo($_SERVER['REQUEST_URI'], PATHINFO_FILENAME ) == "info"){
+			header('location:https://tearounder.app/TheLoremIpsum/info.html');
+			return;
+		}
 		switch (pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME)){
 			case "/ThreeCups":
 				header('location:https://tearounder.app/ThreeCups/menu.html');
@@ -8,6 +12,7 @@
 				header('location:https://tearounder.app/TheLoremIpsum/menu.html');
 				break;
 			default:
+				header('location:https://tearounder.app/index.html');
 		}
 	} else {
 		header('location:https://tearounder.app/index.html');
