6190265aef422a58a9a5bad9a96519d737154c9d
Phil Ganney <philipganney@gmail.com>
2022-06-03 16:47:46 +0100

    Venue Configurator: Testing more secure PHP

--- configureVenue.php
+++ configureVenue.php
@@ -7,10 +7,26 @@
 <body>
     <h1>Thank You</h1>
     <p>Here is the information you have submitted:</p>
+	<?php
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  $venueName = test_input($_POST["venueName"]);
+	  $venueShortName = test_input($_POST["venueShortName"]);
+	  $welcomeMessage = test_input($_POST["welcomeMessage"]);
+	}
+
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+	?>
+	
     <ol>
-        <li><em>Name:</em> <?php echo $_POST["venueName"]?></li>
-        <li><em>Email:</em> <?php echo $_POST["venueShortName"]?></li>
-        <li><em>Subject:</em> <?php echo $_POST["welcomeMessage"]?></li>
+        <li><em>Name:</em> <?php echo $venueName?></li>
+        <li><em>Email:</em> <?php echo $venueShortName?></li>
+        <li><em>Subject:</em> <?php echo $welcomeMessage?></li>
     </ol>
 </body>
 </html>
