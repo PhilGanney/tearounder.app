2568085072f4668a1cf294989959e1c7bc036380
Phil Ganney <philipganney@gmail.com>
2022-06-10 11:21:28 +0100

    Testing if I can call PHP via AJAX

---
+++ importMenuFromWoo.php
@@ -0,0 +1,10 @@
+<?php
+echo "PHP script triggered";
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
