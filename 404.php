dd7c1ef2123d2bec60aa2fc19259ebc86bcb17bc
Phil Ganney <philipganney@gmail.com>
2023-01-23 10:45:21 +0000

    404.php now only redirects to home

--- 404.php
+++ 404.php
@@ -1,20 +1,5 @@
 <?php
-	if (isset( $_SERVER['REQUEST_URI'])) {
-		if (pathinfo($_SERVER['REQUEST_URI'], PATHINFO_FILENAME ) == "info"){
-			header('location:https://tearounder.app/TheLoremIpsum/info.html');
-			return;
-		}
-		switch (strtolower(pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME))){
-			case "/threecups":
-				header('location:https://tearounder.app/ThreeCups/menu.html');
-				break;
-			case "/theloremipsum":
-				header('location:https://tearounder.app/TheLoremIpsum/menu.html');
-				break;
-			default:
-				header('location:https://tearounder.app/index.html');
-		}
-	} else {
-		header('location:https://tearounder.app/index.html');
-	}
+
+	header('location:https://tearounder.app/index.html');
+
 ?>
